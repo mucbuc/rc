1a9f839db3dab6ee7f99f9f0dc7d40c17a467d85
markymark <smallitle@hotmail.com>
2014-11-29 05:47:46 -0800

    micro-box and express 4

--- app.js
+++ app.js
@@ -6,11 +6,16 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , http = require('http')
   , assert = require( 'assert' )
+  , logger = require( 'morgan' )
   , Logic = require( './lib/logic.js' ).Logic
   , upload = require( './routes/upload' ).upload
-  , config = require( './config.json' );
+  , config = require( './config.json' )
+  , methodOverride = require('method-override')
+  , bodyParser = require('body-parser')
+  , errorHandler = require('errorhandler')
+  , multer = require('multer')
+  , server;
 
 assert( typeof config !== 'undefined' );
 
@@ -20,21 +25,21 @@ var app = express();
 app.set('port', process.env.PORT || process.argv[2] || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(multer());
 
 if (config.username.length || config.password.length) {
 	app.use(express.basicAuth( config.username, config.password ));
 }
-
-app.use(app.router);
+  
 app.use(express.static(__dirname));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 app.get('/', routes.index);
@@ -43,9 +48,7 @@ if (config.upload) {
 	app.post('/*', upload );
 }
 
-server = http.createServer(app);
-
-server.listen(app.get('port'), function(){
+server = app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
